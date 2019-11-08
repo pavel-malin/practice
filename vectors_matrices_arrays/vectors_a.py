c439444b6ce8af4c0b2b62a5e340ad2f917206bd
pavel-malin <kurchevskijpavel@gmail.com>
2019-11-08 11:10:18 +0300

    vector

---
+++ vectors_matrices_arrays/vectors_a.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+
+# Creating a vector as a string
+vector_row = np.array([1, 2, 3])
+
+# Creating a vector as a column
+vector_column = np.array([[1],
+						  [2],
+						  [3]])
