6610cfd89381b86aa289d9bd8a54d4395c6dedb5
pavel-malin <kurchevskijpavel@gmail.com>
2019-11-10 15:54:31 +0300

    vector matrix, sparse_matrix matrix

---
+++ vectors_matrices_arrays/vectors_b.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+
+# Create row vector
+vector = np.array([1, 2, 3, 4, 5, 6])
+
+# Create matrix
+matrix = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+
+# Select the third element of the vector [3]
+print(vector[2])
+
+# Select second row, second column [5]
+print(matrix[1, 1])
