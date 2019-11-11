6d091cf9df67e0e8b08c4b55f6cd9f159b05a80e
pavel-malin <kurchevskijpavel@gmail.com>
2019-11-11 11:21:47 +0300

    vector matrix, sparse_matrix matrix item selection

--- vectors_matrices_arrays/vectors_b.py
+++ vectors_matrices_arrays/vectors_b.py
@@ -12,3 +12,22 @@ print(vector[2])
 
 # Select second row, second column [5]
 print(matrix[1, 1])
+
+# Select all elements of the vector (array([1, 2, 3, 4, 5, 6]))
+print(vector[:])
+
+# Select everything up to and including the third item (array([1, 2, 3]))
+print(vector[:3])
+
+# Select everything after the third item (array([4, 5, 6]))
+print(vector[3:])
+
+# Select last item (6)
+print([-1])
+
+# Select the first two rows and all columns of the matrix
+# (array([[1, 2, 3], [4, 5, 6,]])
+print(matrix[:2,:])
+
+# Select all rows and second column (array([[2], [5], [8]])
+print(matrix[:, 1:2])
